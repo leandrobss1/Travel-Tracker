983c1bb6ed1ba978b8064cc2a8eb50a65c9271bf
Leandro Borges <leandrinho_216@hotmail.com>
2025-09-18 00:23:06 -0300

    Finalizado projeto e solução contra alguns possíveis erros de input

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use(express.static('public'));
 
 async function checkVisisted() {
 	const result = await db.query('SELECT country_code FROM visited_countries');
-
 	let countries = [];
 	result.rows.forEach((country) => {
 		countries.push(country.country_code);
@@ -35,19 +34,37 @@ app.get('/', async (req, res) => {
 app.post('/add', async (req, res) => {
 	const input = req.body['country'];
 
-	const result = await db.query(
-		'SELECT country_code FROM countries WHERE country_name = $1',
-		[input]
-	);
+	try {
+		const result = await db.query(
+			"SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+			[input.toLowerCase()]
+		);
 
-	if (result.rows.length !== 0) {
 		const data = result.rows[0];
 		const countryCode = data.country_code;
-
-		await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [
-			countryCode,
-		]);
-		res.redirect('/');
+		try {
+			await db.query(
+				'INSERT INTO visited_countries (country_code) VALUES ($1)',
+				[countryCode]
+			);
+			res.redirect('/');
+		} catch (err) {
+			console.log(err);
+			const countries = await checkVisisted();
+			res.render('index.ejs', {
+				countries: countries,
+				total: countries.length,
+				error: 'Esse país já foi adicionado, tente outro novamente.',
+			});
+		}
+	} catch (err) {
+		console.log(err);
+		const countries = await checkVisisted();
+		res.render('index.ejs', {
+			countries: countries,
+			total: countries.length,
+			error: 'O nome desse país não existe, tente outro novamente.',
+		});
 	}
 });
 
